a1427b1d29a771eff3dd3b3888f0d5b164cb33b2
华旦 王 <799260942@qq.com>
2023-11-20 01:29:47 +0800

    feat:添加好了基本页面，配置了router，用router-link进行了前端渲染

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <ContentBase>123</ContentBase>
+</template>
+
+<script>
+import ContentBase from '../components/ContentBase'
+
+export default {
+  name: 'HomeView',
+  components: {
+    ContentBase,
+  }
+}
+</script>
+
+<style scoped></style>
