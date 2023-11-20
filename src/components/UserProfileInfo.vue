d84fba79836219f97c3aa9bd50a697ba66d9d1d9
华旦 王 <799260942@qq.com>
2023-11-20 17:51:20 +0800

    feat:1.实现了用户发动态页面，关注，取消关注会使粉丝数变化；2.新增了发帖功能；

---
+++ src/components/UserProfileInfo.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="card">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-3">
+                    <img class="img-fluid" src="https://cdn.acwing.com/media/user/profile/photo/97530_lg_4b1ae05c05.jpg"
+                        alt="">
+                </div>
+                <div class="col-9">
+                    <div class="username">
+                        {{ fullName }}
+                    </div>
+                    <div class="fans">粉丝数：{{ user.followerCount }}</div>
+                    <button v-on:click="follow" v-if="!user.is_followed" type="button"
+                        class="btn btn-secondary btn-sm">+关注</button>
+                    <button v-on:click="unfollow" v-if="user.is_followed" type="button"
+                        class="btn btn-secondary btn-sm">+取消关注</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { computed } from 'vue';
+export default {
+    name: "UserProfileInfo",
+    props: {
+        user: {
+            type: Object,
+            required: true,
+        },
+    },
+    setup(props, context) {
+        let fullName = computed(() => props.user.lastName + " " + props.user.firstName)
+        const follow = () => {
+            context.emit("follow123")
+        }
+        const unfollow = () => {
+            context.emit("unfollow1234")
+        }
+        return {
+            fullName,
+            follow,
+            unfollow
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+img {
+    border-radius: 50%;
+}
+
+.username {
+    font-weight: bold;
+}
+
+.fans {
+    font-size: 12px;
+    color: gray;
+}
+
+button {
+    padding: 2px 4px;
+    font-size: 12px;
+}
+</style>
--- src/views/UserProfileView.vue
+++ src/views/UserProfileView.vue
@@ -1,17 +1,100 @@
 <template>
     <ContentBase>
-        用户动态
+        <div class="row">
+            <div class="col-3">
+                <UserProfileInfo @follow123="follow" @unfollow1234="unfollow" v-bind:user="user"></UserProfileInfo>
+                <UserProfileWrite @post_post_123="post_a_post"></UserProfileWrite>
+            </div>
+            <div class="col-9">
+                <UserProfilePosts :posts="posts"></UserProfilePosts>
+            </div>
+        </div>
     </ContentBase>
 </template>
   
 <script>
 
 import ContentBase from '../components/ContentBase';
+import UserProfileInfo from '../components/UserProfileInfo';
+import UserProfilePosts from '../components/UserProfilePosts'
+import UserProfileWrite from '../components/UserProfileWrite';
+import { reactive } from 'vue'
 
 export default {
     name: 'UserProfileView',
     components: {
-        ContentBase
+        ContentBase,
+        UserProfileInfo,
+        UserProfilePosts,
+        UserProfileWrite
+    },
+    setup() {
+        const user = reactive({
+            username: "wanghuadan",
+            lastName: "Wang",
+            firstName: "HuaDan",
+            followerCount: 100,
+            is_followed: false
+        })
+
+        const posts = reactive({
+            count: 4,
+            posts: [
+                {
+                    id: 1,
+                    userId: 1,
+                    content: "今天上了Web课"
+                },
+                {
+                    id: 2,
+                    userId: 1,
+                    content: "今天上了Web课"
+                },
+                {
+                    id: 3,
+                    userId: 1,
+                    content: "今天上了Web课"
+                },
+                {
+                    id: 4,
+                    userId: 1,
+                    content: "今天上了Web课"
+                },
+            ]
+        })
+        const follow = () => {
+            if (user.is_followed) {
+                return
+            }
+            user.is_followed = true
+            user.followerCount++
+        }
+
+        const unfollow = () => {
+            if (!user.is_followed) {
+                return
+            }
+            user.is_followed = false
+            user.followerCount--
+        }
+
+        const post_a_post = (content) => {
+            posts.count++
+            posts.posts.unshift({
+                id: posts.count,
+                userId: 1,
+                content: content
+            })
+
+        }
+
+        return {
+            user,
+            follow,
+            unfollow,
+            posts,
+            post_a_post
+        }
     }
 }
 </script>
